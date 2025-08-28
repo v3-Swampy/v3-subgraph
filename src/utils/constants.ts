c1732cc4067db55b89097609e70d400ad11ba306
0xforce <0xforce@proton.me>
2025-08-28 10:47:50 +0800

    update address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x22ed7ae8299dffc0bedebd7dd0640688e8ad5c3b'; // TOUPDATE
+export const FACTORY_ADDRESS = '0xbd962b897233ea3c2322d45d97d2e8e42e4af974'; // TOUPDATE
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
